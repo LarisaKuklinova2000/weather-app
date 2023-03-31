7ae1a08e7b0f85c733b7371fd96cd1fc2ffe9784
Ivan <gnosticism100500@gmail.com>
2023-03-31 11:10:53 +0500

    init commit

---
+++ dist/script.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!************************!*\
+  !*** ./src/js/main.js ***!
+  \************************/
+console.log(123);
+/******/ })()
+;
+//# sourceMappingURL=script.js.map
